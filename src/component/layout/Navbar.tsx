d392bf744159fa637923243033492935a41f1d1e
Saly222 <139831340+Saly222@users.noreply.github.com>
2026-09-09 22:21:36 +1000

    align logo

--- src/component/layout/Navbar.tsx
+++ src/component/layout/Navbar.tsx
@@ -27,11 +27,15 @@ function NavItem({
   return (
     <div
       onClick={onClick}
-      className={`flex items-center gap-3 p-2 rounded-lg cursor-pointer ${active ? "bg-primary" : "hover:bg-primary/20"} transition-colors`}
+      className={`flex items-center gap-3 p-2 rounded-lg cursor-pointer ${
+        active ? "bg-primary" : "hover:bg-primary/20"
+      } transition-colors`}
     >
       <Icon className={`w-6 h-6 ${active ? "text-black" : "text-white/70"}`} />
       <p
-        className={`font-display ${active ? "text-black font-bold" : "text-white/90"}`}
+        className={`font-display ${
+          active ? "text-black font-bold" : "text-white/90"
+        }`}
       >
         {label}
       </p>
@@ -44,7 +48,6 @@ type NavbarProps = React.ComponentProps<"nav">;
 function Navbar({ className, ...props }: NavbarProps) {
   const [activeItem, setActiveItem] = useState("Dashboard");
   const [open, setOpen] = useState(false);
-
 
   useEffect(() => {
     if (!open) {
@@ -67,16 +70,18 @@ function Navbar({ className, ...props }: NavbarProps) {
   const navigateLogin = () => {
     navigate("/login");
   };
-  const navigateHomePage= () => {
+  const navigateHomePage = () => {
     navigate("/Home");
   };
-  const { user,loading} = useAuth();
+  const { user, loading } = useAuth();
   const displayName = user?.userName ?? user?.email ?? "User";
+  const signOutResult = await handleSignOut(email, password);
 
-      
   return (
     <nav
-      className={`relative bg-primary-dark flex flex-col h-full md:border-r-[0.5px] border-white/15 ${className ?? ""}`.trim()}
+      className={`relative bg-primary-dark flex flex-col h-full md:border-r-[0.5px] border-white/15 ${
+        className ?? ""
+      }`.trim()}
       {...props}
     >
       <div className="fixed top-0 w-screen flex items-center justify-between border-b-[0.5px] border-white/15 bg-primary-dark/60 px-4 py-4 backdrop-blur-xl">
@@ -100,13 +105,38 @@ function Navbar({ className, ...props }: NavbarProps) {
             </div>
           )}
         </button>
-        <div className="flex items-center gap-2">
-          <Dumbbell className="w-8 h-8 p-1 rounded bg-primary text-background-dark" />
-          <p className="font-display text-white font-bold text-2xl leading-none">
-            FitTrack
-          </p>
+
+        {/* Logo centered  */}
+
+        <div className="flex items-center">
+          <div className="absolute left-1/2 -translate-x-1/2 flex items-center gap-2">
+            <Dumbbell className="w-8 h-8 p-1 rounded bg-primary text-background-dark" />
+            <p className="font-display text-white font-bold text-2xl leading-none">
+              FitTrack
+            </p>
+          </div>
+          {!loading && !user ? (
+            <div className="py-2 bg-primary rounded-2xl w-20 text-center">
+              <button
+                className="text-black font-bold items-center"
+                type="button"
+                onClick={navigateLogin}
+              >
+                Join us
+              </button>
+            </div>
+          ) : (
+            <div className="flex items-center gap-3 mr-10">
+              <div className="size-10 bg-white/15 p-1.75 rounded-3xl">
+                {/* features: add user name after auth*/}
+                <button>
+                  <CircleUserRound className="text-primary" />
+                </button>
+              </div>
+              <p className="text-white font-bold">{displayName}</p>
+            </div>
+          )}
         </div>
-        <div className="w-10" />
       </div>
 
       {open && (
@@ -162,21 +192,21 @@ function Navbar({ className, ...props }: NavbarProps) {
               }}
             />
             <span className="border-b-[0.5px] border-white/15 w-auto block mt-105 md:mt-150 lg:mt-115 lg:inline"></span>
-            {!loading && !user ? (
+            {/* !loading && !user ? (
             <div className="py-2 bg-primary rounded-2xl w-20 text-center">
               <button className="text-black font-bold items-center" type="button" onClick={navigateLogin}>Join us</button>
             </div>
             ) :(
               <div className="flex items-center gap-8">
                 <div className="size-10 bg-white/15 p-2 rounded-3xl mt-6">
-                  {/* features: add user name after auth*/}
+                  // features: add user name after auth
                   <button >
                     <CircleUserRound className="text-primary" />
                   </button>
                 </div>
                 <p className="text-white font-bold mt-6">{displayName}</p>
               </div>
-            )}
+            ) */}
           </div>
         </div>
       )}
